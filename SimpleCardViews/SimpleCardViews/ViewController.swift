0d5e4f06d2f0f4bbc76013e2f4d7a85b61795296
Margaret E. Ikeda <margaretei@gmail.com>
2016-12-17 22:58:26 -0500

    first commit

---
+++ SimpleCardViews/SimpleCardViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCardViews
+//
+//  Created by Margaret Ikeda on 12/17/16.
+//  Copyright © 2016 Margaret Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
