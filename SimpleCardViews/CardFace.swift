a75b43b334a81ac43b83a62fc1f5891d38979150
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-12-18 13:55:25 -0500

    made function signature more swifty

--- SimpleCardViews/CardFace.swift
+++ SimpleCardViews/CardFace.swift
@@ -36,7 +36,7 @@ class CardFace: UIView {
         }
     }
     
-    static func labelCards(cards: [CardFace]) {
+    static func label(_ cards: [CardFace]) {
         let url = [URL(string: "https://s-media-cache-ak0.pinimg.com/originals/bd/91/01/bd9101d7b12356ab4bfa9e45ef67af68.jpg"), URL(string: "http://haseebq.com/wordpress/wp-content/uploads/2014/01/things.jpg"), URL(string: "http://3.bp.blogspot.com/-LRAWJ9E1gDc/VMY4iZyT7oI/AAAAAAAAB-E/YSgQwWDO5HI/s1600/7.png"), URL(string: "http://totally-90s.com/wp-content/uploads/2014/02/tetris4.jpg"), URL(string: "http://www.homespunhearth.com/images/Newsletter/Xmas-5.jpg"), URL(string: "http://www.wpclipart.com/signs_symbol/shapes/star_6_point.png"), URL(string: "https://www.drpeppersnapplegroup.com/smedia/www/2011/04/12/img-7up-breakout-box-4-spot_144621699059.png"), URL(string: "http://www.table8.org/8ball_big.jpg"), URL(string: "https://s-media-cache-ak0.pinimg.com/originals/b2/d3/03/b2d30374872aa40ae70439a8539ae8bf.jpg"), URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Interlaced_ten-point_star_in_irregular_decagon.svg/814px-Interlaced_ten-point_star_in_irregular_decagon.svg.png"), URL(string: "http://pre01.deviantart.net/0968/th/pre/f/2009/180/b/6/jack_skellington_6_by_carlibux.jpg"), URL(string: "http://img.wennermedia.com/article-leads-vertical-380/beyonce-zoom-f0ce8492-6c83-4074-a7d9-963aea4bfadf.jpg"), URL(string: "http://www.tastyburger.com/wp-content/themes/tastyBurger/images/home/img-large-burger.jpg")]
         let suits = ["♠︎","♥︎","♣︎","♦︎"]
         let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
--- SimpleCardViews/SimpleCardViews/ViewController.swift
+++ SimpleCardViews/SimpleCardViews/ViewController.swift
@@ -29,6 +29,6 @@ class ViewController: UIViewController {
         
         let cards = [card1!,card2!,card3!,card4!,card5!,card6!,card7!,card8!,card9!,card10!,cardJ!,cardQ!,cardK!]
         
-        CardFace.labelCards(cards: cards)
+        CardFace.label(cards)
     }
 }
