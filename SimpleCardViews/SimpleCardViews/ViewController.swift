698e292178795edf327f55c12dbaf7c18996a45a
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-12-18 11:37:46 -0500

    set up storyboard to display 13 cards

--- SimpleCardViews/SimpleCardViews/ViewController.swift
+++ SimpleCardViews/SimpleCardViews/ViewController.swift
@@ -11,20 +11,35 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var card1: CardFace!
-    let url = URL(string: "http://vignette4.wikia.nocookie.net/scribblenauts/images/4/42/Crash_Test_Dummy.png/revision/latest?cb=20130309213400")
+    @IBOutlet weak var card2: CardFace!
+    @IBOutlet weak var card3: CardFace!
+    @IBOutlet weak var card4: CardFace!
+    @IBOutlet weak var card5: CardFace!
+    @IBOutlet weak var card6: CardFace!
+    @IBOutlet weak var card7: CardFace!
+    @IBOutlet weak var card8: CardFace!
+    @IBOutlet weak var card9: CardFace!
+    @IBOutlet weak var card10: CardFace!
+    @IBOutlet weak var cardJ: CardFace!
+    @IBOutlet weak var cardQ: CardFace!
+    @IBOutlet weak var cardK: CardFace!
+    
+    let url = [URL(string: "https://s-media-cache-ak0.pinimg.com/originals/bd/91/01/bd9101d7b12356ab4bfa9e45ef67af68.jpg")]
+    let suits = ["♠︎","♣︎","♥︎","♦︎"]
+    let numbers = ["A","1","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        card1.numbering.text = "A"
-        card1.numberingReversed.text = "A"
-        card1.suit.text = "♠︎"
-        card1.suitReversed.text = "♠︎"
+        card1.numbering.text = numbers[0]
+        card1.numberingReversed.text = card1.numbering.text
+        card1.suit.text = suits[0]
+        card1.suitReversed.text = card1.suit.text
         
         card1.numberingReversed.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
         card1.suitReversed.transform = CGAffineTransform(rotationAngle: -CGFloat.pi)
         
-        downloadImage(url: url!)
+        downloadImage(url: url[0]!)
         
     }
 
