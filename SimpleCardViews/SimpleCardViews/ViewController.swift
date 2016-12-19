778e332cdfcfe2e03a65d577df661ccfc940dfa2
Victor Zhong <viczhong@gmail.com>
2016-12-19 03:35:17 -0500

    That's all, folks.

--- SimpleCardViews/SimpleCardViews/ViewController.swift
+++ SimpleCardViews/SimpleCardViews/ViewController.swift
@@ -9,18 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var topLeft: UIView!
-    @IBOutlet weak var topRight: UIView!
-    @IBOutlet weak var bottomLeft: UIView!
-    @IBOutlet weak var bottomRight: UIView!
+    
+    @IBOutlet weak var topLeft: CardView!
+    @IBOutlet weak var topRight: CardView!
+    @IBOutlet weak var bottomLeft: CardView!
+    @IBOutlet weak var bottomRight: CardView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        drawViews()
     }
-
-
+    
+    func drawViews() {
+        let views = [topLeft, topRight, bottomLeft, bottomRight]
+        let deck: [(card: String, suit: String)] = [
+            ("A♠︎", "spade"),
+            ("A♥︎", "heart"),
+            ("A♣︎", "club"),
+            ("A♦︎", "diamond")
+        ]
+        
+        for (index,card) in views.enumerated() {
+            let drawnCard = deck[index]
+           
+            card?.topLabel.text = drawnCard.card
+            card?.bottomLabel.text = drawnCard.card
+         
+            if drawnCard.suit == "diamond" || drawnCard.suit == "heart" {
+                card?.topLabel.textColor = .red
+                card?.bottomLabel.textColor = .red
+            }
+            
+            card?.cardImage.image = UIImage(named: drawnCard.suit)
+        }
+    }
 }
 
