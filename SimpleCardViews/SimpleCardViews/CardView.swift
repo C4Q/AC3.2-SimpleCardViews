5c4727caae2aabbaaa86f7799fe635f48726224b
Annie <annie.mmt@gmail.com>
2016-12-19 17:42:34 -0500

    update

--- SimpleCardViews/SimpleCardViews/CardView.swift
+++ SimpleCardViews/SimpleCardViews/CardView.swift
@@ -1,3 +1,4 @@
+
 //
 //  CardView.swift
 //  SimpleCardViews
@@ -19,17 +20,17 @@ class CardView: UIView {
         }
     }
     var displayView: UIView!
-
+    
     @IBInspectable var cornerRadius: Double = 0.0
     @IBInspectable var cornerWidth: Double = 0.0
     @IBInspectable var borderColor: UIColor = UIColor.black
     
     @IBOutlet var contentView: UIView!
-
+    
     @IBOutlet weak var cardImage: UIImageView!
     @IBOutlet weak var topLeftLabel: UILabel!
     @IBOutlet weak var bottomRightLabel: UILabel!
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -44,19 +45,12 @@ class CardView: UIView {
             cardImage.layer.borderWidth = 1.0
             cardImage.layer.borderColor = UIColor.gray.cgColor
             
-//            contentView.clipsToBounds = true
-//            contentView.layer.borderWidth = 1.0
-//            contentView.layer.borderColor = UIColor.black.cgColor
-//            contentView.layer.cornerRadius = 5.0
-//r             self.backgroundColor = .clear
+            contentView.clipsToBounds = true
+            contentView.layer.borderWidth = 1.0
+            contentView.layer.borderColor = UIColor.black.cgColor
+            contentView.layer.cornerRadius = 5.0
+            self.backgroundColor = .clear
         }
     }
-    
-    override func awakeFromNib() {
-            contentView.layer.cornerRadius = CGFloat(cornerRradius)
-            contentView.layer.borderWidth = CGFloat(corner__Width)
-            contentView.layer.borderColor = borderColor.cgColor
-
-        }
-    }
+}
 
