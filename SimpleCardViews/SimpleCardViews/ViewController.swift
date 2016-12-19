910eedf4de116f45263447afd127913c1d3ee68b
Eashir Arafat <eashirarafat@gmail.com>
2016-12-18 19:01:08 -0500

    Made custom view from nib

---
+++ SimpleCardViews/SimpleCardViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCardViews
+//
+//  Created by Eashir Arafat on 12/18/16.
+//  Copyright © 2016 Evan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
