aa47c286d57d4a08b1b7a38c52b07e257d78a3f6
Victor Zhong <viczhong@gmail.com>
2016-12-19 02:57:18 -0500

    Set up views and nibs. What now?

---
+++ SimpleCardViews/SimpleCardViews/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SimpleCardViews
+//
+//  Created by Victor Zhong on 12/18/16.
+//  Copyright © 2016 Victor Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topLeft: UIView!
+    @IBOutlet weak var topRight: UIView!
+    @IBOutlet weak var bottomLeft: UIView!
+    @IBOutlet weak var bottomRight: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
